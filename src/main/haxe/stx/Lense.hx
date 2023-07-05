3839c285c4133ccb62aa52f8658b7855181224f7
0b1kn00b <polysemantic@gmail.com>
2023-07-05 09:38:24 +0200

    better membership test

--- src/main/haxe/stx/Lense.hx
+++ src/main/haxe/stx/Lense.hx
@@ -5,8 +5,9 @@ using stx.Nano;
 typedef LenseFailureSum   = stx.fail.LenseFailure.LenseFailureSum;
 typedef LenseFailure      = stx.fail.LenseFailure;
 
-typedef LExpr<K,V>    = stx.lense.LExpr<K,V>;
-typedef LExprSum<K,V> = stx.lense.LExpr.LExprSum<K,V>;
+typedef LenseReader       = stx.lense.Reader;
+typedef LExpr<K,V>        = stx.lense.LExpr<K,V>;
+typedef LExprSum<K,V>     = stx.lense.LExpr.LExprSum<K,V>;
 
 class Lense{
   static public function lense(self:Wildcard){
--- src/main/haxe/stx/lense/import.hx
+++ src/main/haxe/stx/lense/import.hx
@@ -4,3 +4,7 @@ using stx.Pico;
 using stx.Nano;
 using stx.Assert;
 using stx.Lense;
+using stx.Ds;
+using stx.Fail;
+
+using eu.ohmrun.Pml;
